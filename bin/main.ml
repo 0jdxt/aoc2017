2fc4c48bfb4240c4d3f1db13bd97849bef2b14fe
Jay <jytrn@protonmail.com>
2026-03-25 20:59:58 +0000

    setup and day1

---
+++ bin/main.ml
@@ -0,0 +1,5 @@
+open Aocaml
+
+let () =
+  Printf.printf "1 ╶┰╴%d\n" (Day1.part1 ());
+  Printf.printf "   ┖╴%d\n" (Day1.part2 ())
---
+++ lib/day1.ml
@@ -0,0 +1,22 @@
+open Batteries
+
+let digits =
+  File.lines_of "data/day1.txt"
+  |> Enum.get_exn |> String.to_seq
+  |> Seq.map (fun c -> int_of_char c - int_of_char '0')
+  |> Array.of_seq
+
+let part1 () =
+  let len = Array.length digits in
+  digits
+  |> Array.mapi (fun i x ->
+      let j = if i = len - 1 then 0 else i + 1 in
+      if x = digits.(j) then x else 0)
+  |> Array.sum
+
+let part2 () =
+  let len = Array.length digits in
+  digits
+  |> Array.mapi (fun i x ->
+      if x = digits.((i + (len / 2)) mod len) then x else 0)
+  |> Array.sum
